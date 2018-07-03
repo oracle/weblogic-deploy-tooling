74e22f56d49ae6aed6665c5baddc0c681a424833
Richard Killen <richard.killen@oracle.com>
2018-07-03 17:26:57 -0400

    Issue #134 - Use properties file from mounted volume; use WLS quick sample image; use production start mode

--- samples/docker-domain/container-scripts/startWLSDomain.sh
+++ samples/docker-domain/container-scripts/startWLSDomain.sh
@@ -6,11 +6,36 @@
 #
 # Start the Domain.
 
-# determine the domain name. there is only one domain directory.
-export DOMAIN_NAME=`ls /u01/oracle/user_projects/domains | head -1`
+PROPERTIES_FILE=/u01/oracle/properties/domain.properties
+if [ ! -e "$PROPERTIES_FILE" ]; then
+    echo "A properties file with the username and password needs to be supplied."
+    exit
+fi
+
+DOMAIN_NAME=`awk '{print $1}' $PROPERTIES_FILE | grep ^DOMAIN_NAME= | cut -d "=" -f2`
+if [ -z "$DOMAIN_NAME" ]; then
+    echo "The domain name is blank.  The domain name must be set in the properties file."
+    exit
+fi
+
+USER=`awk '{print $1}' $PROPERTIES_FILE | grep ^username= | cut -d "=" -f2`
+if [ -z "$USER" ]; then
+    echo "The domain username is blank.  The Admin username must be set in the properties file."
+    exit
+fi
+
+PASS=`awk '{print $1}' $PROPERTIES_FILE | grep ^password= | cut -d "=" -f2`
+if [ -z "$PASS" ]; then
+    echo "The domain password is blank.  The Admin password must be set in the properties file."
+    exit
+fi
 
 #Define DOMAIN_HOME
 export DOMAIN_HOME=/u01/oracle/user_projects/domains/$DOMAIN_NAME
+
+mkdir -p ${DOMAIN_HOME}/servers/AdminServer/security/
+echo "username=${USER}" >> $DOMAIN_HOME/servers/AdminServer/security/boot.properties
+echo "password=${PASS}" >> $DOMAIN_HOME/servers/AdminServer/security/boot.properties
 
 # Start Admin Server and tail the logs
 ${DOMAIN_HOME}/startWebLogic.sh
