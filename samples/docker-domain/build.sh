db2865105e07f1bceb60b1a98b27306f28063130
Carolyn Rountree <carolyn.rountree@oracle.com>
2018-11-28 15:37:50 -0800

    Add prefix ARG to arg variables

--- samples/docker-domain/build.sh
+++ samples/docker-domain/build.sh
@@ -4,13 +4,16 @@
 #
 #Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
-. container-scripts/setEnv.sh ./simple-topology.properties
+
+# parse the ADMIN_HOST, ADMIN_PORT, MS_PORT, and DOMAIN_NAME from the sample properties file and pass
+# as a string of --build-arg in the variable BUILD_ARG
+. container-scripts/setEnv.sh properties/docker-build/domain.properties
 
 
 docker build \
     $BUILD_ARG \
     --build-arg WDT_MODEL=simple-topology.yaml \
-    --build-arg WDT_VARIABLE=simple-topology.properties \
+    --build-arg WDT_VARIABLE=properties/docker-build/domain.properties \
     --build-arg WDT_ARCHIVE=archive.zip \
-    -t 12213-domain-wdt-test .
+    -t 12213-domain-wdt .
 
--- samples/docker-domain/container-scripts/setEnv.sh
+++ samples/docker-domain/container-scripts/setEnv.sh
@@ -2,10 +2,16 @@
 
 # Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
 # The Universal Permissive License (UPL), Version 1.0
- BUILD_ARG=''
- if [ $# > 1 ]; then
-   PROPERTIES_FILE=$1
- fi
+#
+# This example creates the BUILD_ARG environment variable as a string of --build-arg for 
+# the arguments passed on the docker build command. The variable file that is used for the WDT
+# create domain step is the input to this script. This insures that the values persisted
+# as environment variables in the docker image match the configured domain home.
+
+BUILD_ARG=''
+if [ $# > 1 ]; then
+  PROPERTIES_FILE=$1
+fi
 
 if [ ! -e "${PROPERTIES_FILE}" ]; then
     echo "A properties file with variable definitions should be supplied."
@@ -20,34 +26,38 @@ if [ ! -n "$DOMAIN_DIR" ]; then
    fi
 fi
 if [ -n "$DOMAIN_DIR" ]; then
-     export DOMAIN_DIR
-     echo DOMAIN_DIR=$DOMAIN_DIR
-     BUILD_ARG="$BUILD_ARG --build-arg DOMAIN_DIR=$DOMAIN_DIR"
+     DOMAIN_NAME=$DOMAIN_DIR
+     export DOMAIN_NAME
+     echo DOMAIN_NAME=$DOMAIN_NAME
+     BUILD_ARG="$BUILD_ARG --build-arg ARG_DOMAIN_NAME=$DOMAIN_NAME"
 fi
 
 ADMIN_HOST=`awk '{print $1}' $PROPERTIES_FILE | grep ^ADMIN_HOST= | cut -d "=" -f2`
 if [ -n "$ADMIN_HOST" ]; then
     export ADMIN_HOST
     echo ADMIN_HOST=$ADMIN_HOST
-    BUILD_ARG="$BUILD_ARG --build-arg ADMIN_HOST=$ADMIN_HOST"
+    BUILD_ARG="$BUILD_ARG --build-arg ARG_ADMIN_HOST=$ADMIN_HOST"
 fi
 
 ADMIN_PORT=`awk '{print $1}' $PROPERTIES_FILE | grep ^ADMIN_PORT= | cut -d "=" -f2`
 if [ -n "$ADMIN_PORT" ]; then
     export ADMIN_PORT
     echo ADMIN_PORT=$ADMIN_PORT
-    BUILD_ARG="$BUILD_ARG --build-arg ADMIN_PORT=$ADMIN_PORT"
+    BUILD_ARG="$BUILD_ARG --build-arg ARG_ADMIN_PORT=$ADMIN_PORT"
 fi
 
 MS_PORT=`awk '{print $1}' $PROPERTIES_FILE | grep ^MS_PORT= | cut -d "=" -f2`
 if [ -n "$MS_PORT" ]; then
-    export MS_PORT echo MS_PORT=$MS_PORT BUILD_ARG="$BUILD_ARG --build-arg MS_PORT=$MS_PORT" fi
+    export MS_PORT 
+    echo MS_PORT=$MS_PORT
+    BUILD_ARG="$BUILD_ARG --build-arg ARG_MS_PORT=$MS_PORT"
+fi
 
 DEBUG_PORT=`awk '{print $1}' $PROPERTIES_FILE | grep ^DEBUG_PORT= | cut -d "=" -f2`
 if [ -n "$DEBUG_PORT" ]; then
     export DEBUG_PORT
     echo DEBUG_PORT=$DEBUG_PORT
-    BUILD_ARG="$BUILD_ARG --build-arg DEBUG_PORT=$DEBUG_PORT"
+    BUILD_ARG="$BUILD_ARG --build-arg ARG_DEBUG_PORT=$DEBUG_PORT"
 fi
 
 echo BUILD_ARG=$BUILD_ARG
