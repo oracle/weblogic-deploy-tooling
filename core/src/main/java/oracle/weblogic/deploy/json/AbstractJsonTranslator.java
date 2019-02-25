b9609cc2d12d562bf99a152f66eca2179e2607ea
Richard Killen <richard.killen@oracle.com>
2019-02-25 15:00:29 -0600

    Resolve additional JSON escape sequences, including double backslash

--- core/src/main/java/oracle/weblogic/deploy/json/AbstractJsonTranslator.java
+++ core/src/main/java/oracle/weblogic/deploy/json/AbstractJsonTranslator.java
@@ -71,7 +71,7 @@ public abstract class AbstractJsonTranslator extends JSONBaseListener {
      */
     @Override
     public void enterPair(JSONParser.PairContext ctx) {
-        String name = unquoteEmbeddedQuotes(StringUtils.stripQuotes(ctx.STRING().getText()));
+        String name = resolveEscapeSequences(StringUtils.stripQuotes(ctx.STRING().getText()));
         currentPairName.push(name);
     }
 
@@ -169,7 +169,7 @@ public abstract class AbstractJsonTranslator extends JSONBaseListener {
      */
     @Override
     public void enterJsonString(JSONParser.JsonStringContext ctx) {
-        String cleanString = unquoteEmbeddedQuotes(StringUtils.stripQuotes(ctx.STRING().getText()));
+        String cleanString = resolveEscapeSequences(StringUtils.stripQuotes(ctx.STRING().getText()));
         currentScalarValue = new PyString(cleanString);
         currentValueType.push(ValueType.SCALAR);
     }
@@ -347,10 +347,17 @@ public abstract class AbstractJsonTranslator extends JSONBaseListener {
         }
     }
 
-    private static String unquoteEmbeddedQuotes(String text) {
+    private static String resolveEscapeSequences(String text) {
         String result = text;
         if (!StringUtils.isEmpty(text)) {
             result = text.replace("\\\"", "\"");
+            result = result.replace("\\\\", "\\");
+            result = result.replace("\\b", "\b");
+            result = result.replace("\\f", "\f");
+            result = result.replace("\\n", "\n");
+            result = result.replace("\\r", "\r");
+            result = result.replace("\\t", "\t");
+            result = result.replace("\\/", "/");
         }
         return result;
     }
