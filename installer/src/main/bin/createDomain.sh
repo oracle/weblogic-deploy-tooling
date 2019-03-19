0439065d9a9d5cf6797767b2c785e9a6d9fbb404
Richard Killen <richard.killen@oracle.com>
2019-03-19 14:52:14 -0500

    Issue #287 - Updated copyrights

--- installer/src/main/bin/createDomain.sh
+++ installer/src/main/bin/createDomain.sh
@@ -2,7 +2,7 @@
 # *****************************************************************************
 # createDomain.sh
 #
-# Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
 # The Universal Permissive License (UPL), Version 1.0
 #
 #     NAME
