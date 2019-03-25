1d1a25236db2510fb7f39051fb4aad8fced58fe6
johnny.shum@oracle.com <johnny.shum@oracle.com>
2019-03-25 15:06:01 -0500

    add singleton service support

--- core/src/main/python/wlsdeploy/tool/deploy/resources_deployer.py
+++ core/src/main/python/wlsdeploy/tool/deploy/resources_deployer.py
@@ -72,6 +72,7 @@ class ResourcesDeployer(Deployer):
 
         common_deployer.add_coherence_clusters(self._resources, location)
         common_deployer.add_webapp_container(self._resources, location)
+        common_deployer.add_singleton_service(self._resources, location)
         return
 
     def _add_startup_classes(self, location):
