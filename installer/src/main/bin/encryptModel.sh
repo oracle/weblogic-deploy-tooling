6b035cf50f5fe8745af3ee6b8c1a7c2796743e98
Rosemary Marano <rosemary.marano@oracle.com>
2019-06-25 11:25:04 -0400

    update copyright statements (#403)

--- installer/src/main/bin/encryptModel.sh
+++ installer/src/main/bin/encryptModel.sh
@@ -2,8 +2,8 @@
 # *****************************************************************************
 # encryptModel.sh
 #
-# Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
-# The Universal Permissive License (UPL), Version 1.0
+# Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
 #     NAME
 #       encryptModel.sh - WLS Deploy tool to encrypt the password fields in
