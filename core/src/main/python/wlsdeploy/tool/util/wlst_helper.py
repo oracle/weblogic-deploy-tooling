333e217d66a75acf91cc75142aa55eb3d3653adf
Richard Killen <richard.killen@oracle.com>
2019-08-15 15:10:03 -0500

    JIRA WDT-349 - Add path information to logging for error setting attribute

--- core/src/main/python/wlsdeploy/tool/util/wlst_helper.py
+++ core/src/main/python/wlsdeploy/tool/util/wlst_helper.py
@@ -147,11 +147,13 @@ class WlstHelper(object):
         try:
             wlst_helper.set(attribute_name, attribute_value)
         except PyWLSTException, pwe:
+            path = self.get_pwd()
             log_value = attribute_value
             if masked:
                 log_value = '<masked>'
-            ex = exception_helper.create_exception(self.__exception_type, 'WLSDPLY-19102', attribute_name, log_value,
-                                                   pwe.getLocalizedMessage(), error=pwe)
+
+            ex = exception_helper.create_exception(self.__exception_type, 'WLSDPLY-19102', attribute_name, path,
+                                                   log_value, pwe.getLocalizedMessage(), error=pwe)
             self.__logger.throwing(ex, class_name=self.__class_name, method_name=_method_name)
             raise ex
         return
@@ -169,11 +171,13 @@ class WlstHelper(object):
         try:
             wlst_helper.set_with_cmo(attribute_name, attribute_value, masked=masked)
         except PyWLSTException, pwe:
+            path = self.get_pwd()
             log_value = attribute_value
             if masked:
                 log_value = '<masked>'
-            ex = exception_helper.create_exception(self.__exception_type, 'WLSDPLY-19136', attribute_name, log_value,
-                                                   pwe.getLocalizedMessage(), error=pwe)
+
+            ex = exception_helper.create_exception(self.__exception_type, 'WLSDPLY-19136', attribute_name, path,
+                                                   log_value, pwe.getLocalizedMessage(), error=pwe)
             self.__logger.throwing(ex, class_name=self.__class_name, method_name=_method_name)
             raise ex
         return
