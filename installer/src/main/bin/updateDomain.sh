2f7cd77d143e9ed3e7ac271b0308fa30843a53dd
Richard Killen <richard.killen@oracle.com>
2019-09-27 09:00:29 -0700

    Updated licenses with Corporation

--- installer/src/main/bin/updateDomain.sh
+++ installer/src/main/bin/updateDomain.sh
@@ -2,7 +2,7 @@
 # *****************************************************************************
 # updateDomain.sh
 #
-# Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2017, 2019, Oracle Corporation and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 #     NAME
