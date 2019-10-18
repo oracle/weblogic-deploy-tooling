68b865d9f0b8eeef8d0fa6d1d7a61d37fde32362
Richard Killen <richard.killen@oracle.com>
2019-10-18 09:55:30 -0500

    JIRA WDT-368 - Added multiple archive info to usage in scripts

--- installer/src/main/bin/createDomain.cmd
+++ installer/src/main/bin/createDomain.cmd
@@ -343,8 +343,8 @@ ECHO.
 ECHO         archive_file    - the path to the archive file to use.  If the -model_file
 ECHO                           argument is not specified, the model file in this archive
 ECHO                           will be used.  This can also be specified as a
-ECHO                           comma-separated list of archive files.  The contents in
-ECHO                           each file take precedence over previous files.
+ECHO                           comma-separated list of archive files.  The overlapping contents in
+ECHO                           each archive take precedence over previous archives in the list.
 ECHO.
 ECHO         model_file      - the location of the model file to use.  This can also be specified as a
 ECHO                           comma-separated list of model locations, where each successive model
