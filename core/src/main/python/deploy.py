d90daee82c4d801d606d83c8fc962278ea1a96b7
Richard Killen <richard.killen@oracle.com>
2019-12-06 14:35:47 -0600

    JIRA WDT-380 - Set the typedef correctly to support ODL configuration

--- core/src/main/python/deploy.py
+++ core/src/main/python/deploy.py
@@ -8,19 +8,15 @@ import os
 import sys
 
 from java.io import IOException, PrintStream
-from java.lang import IllegalArgumentException
 from java.lang import String, System
 
 
 from oracle.weblogic.deploy.deploy import DeployException
 from oracle.weblogic.deploy.exception import BundleAwareException
 from oracle.weblogic.deploy.util import CLAException
-from oracle.weblogic.deploy.util import FileUtils
 from oracle.weblogic.deploy.util import TranslateException
 from oracle.weblogic.deploy.util import VariableException
 from oracle.weblogic.deploy.util import WebLogicDeployToolingVersion
-from oracle.weblogic.deploy.util import WLSDeployArchive
-from oracle.weblogic.deploy.util import WLSDeployArchiveIOException
 from oracle.weblogic.deploy.validate import ValidateException
 
 sys.path.append(os.path.dirname(os.path.realpath(sys.argv[0])))
@@ -31,6 +27,7 @@ from wlsdeploy.aliases.wlst_modes import WlstModes
 from wlsdeploy.exception import exception_helper
 from wlsdeploy.exception.expection_types import ExceptionType
 from wlsdeploy.logging.platform_logger import PlatformLogger
+from wlsdeploy.tool.create.domain_typedef import DomainTypedef
 from wlsdeploy.tool.deploy import deployer_utils
 from wlsdeploy.tool.deploy import model_deployer
 from wlsdeploy.tool.validate.validator import Validator
@@ -38,6 +35,7 @@ from wlsdeploy.tool.util import filter_helper
 from wlsdeploy.tool.util.string_output_stream import StringOutputStream
 from wlsdeploy.tool.util.wlst_helper import WlstHelper
 from wlsdeploy.util import cla_helper
+from wlsdeploy.util import dictionary_utils
 from wlsdeploy.util import getcreds
 from wlsdeploy.util import tool_exit
 from wlsdeploy.util import variables
@@ -92,13 +90,25 @@ def __process_args(args):
 
     __verify_required_args_present(required_arg_map)
     __process_model_args(optional_arg_map)
+
+    #
+    # Verify that the domain type is a known type and load its typedef.
+    #
+    domain_type = dictionary_utils.get_element(optional_arg_map, CommandLineArgUtil.DOMAIN_TYPE_SWITCH)
+    if domain_type is None:
+        domain_type = 'WLS'
+    domain_typedef = DomainTypedef(_program_name, domain_type)
+    optional_arg_map[CommandLineArgUtil.DOMAIN_TYPEDEF] = domain_typedef
+
     __wlst_mode = __process_online_args(optional_arg_map)
     __process_encryption_args(optional_arg_map)
 
     combined_arg_map = optional_arg_map.copy()
     combined_arg_map.update(required_arg_map)
 
-    return ModelContext(_program_name, combined_arg_map)
+    model_context = ModelContext(_program_name, combined_arg_map)
+    domain_typedef.set_model_context(model_context)
+    return model_context
 
 
 def __verify_required_args_present(required_arg_map):
