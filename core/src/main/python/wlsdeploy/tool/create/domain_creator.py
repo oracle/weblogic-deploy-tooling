52b743553fa2f650e445170bc07f283ea0d83d2f
crountre <carolyn.rountree@oracle.com>
2020-03-03 14:33:49 -0600

    rearrange create domain extension templates

--- core/src/main/python/wlsdeploy/tool/create/domain_creator.py
+++ core/src/main/python/wlsdeploy/tool/create/domain_creator.py
@@ -174,6 +174,8 @@ class DomainCreator(Creator):
         self.__fail_mt_1221_domain_creation()
         self.__create_domain()
         self.__deploy()
+        self.__set_server_groups()
+        self.__update_domain()
         self.__deploy_after_update()
         self.__create_boot_dot_properties()
 
@@ -342,8 +344,10 @@ class DomainCreator(Creator):
 
         if self.wls_helper.is_select_template_supported():
             self.__create_base_domain_with_select_template(self._domain_home)
+            self.__extend_domain_with_select_template(self._domain_home)
         else:
             self.__create_base_domain(self._domain_home)
+            self.__extend_domain(self._domain_home)
 
         topology_folder_list = self.alias_helper.get_model_topology_top_level_folder_names()
         self.__apply_base_domain_config(topology_folder_list)
@@ -359,28 +363,6 @@ class DomainCreator(Creator):
         self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
         return
 
-    def __extend_domain_svrgrps(self):
-        """
-        Create the domain.
-        :raises: CreateException: if an error occurs
-        """
-        _method_name = '__create_domain'
-
-        self.logger.entering(class_name=self.__class_name, method_name=_method_name)
-        domain_type = self.model_context.get_domain_type()
-        self.logger.info('WLSDPLY-12203', domain_type, class_name=self.__class_name, method_name=_method_name)
-
-        self.wlst_helper.read_domain(self._domain_home)
-
-        if self.wls_helper.is_select_template_supported():
-            self.__extend_domain_with_select_template(self._domain_home)
-        else:
-            self.__extend_domain(self._domain_home)
-
-        self.wlst_helper.update_domain()
-        self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
-        return
-
     def __deploy(self):
         """
         Update the domain with domain attributes, resources and deployments.
@@ -391,15 +373,14 @@ class DomainCreator(Creator):
         self.__set_domain_attributes()
         self._configure_security_configuration()
         self.__deploy_resources_and_apps()
-        self.wlst_helper.update_domain()
-        self.wlst_helper.close_domain()
 
         return
 
     def __deploy_after_update(self):
-
-        self.__extend_domain_svrgrps()
+        _method_name = '__deploy_after_update'
+        self.logger.entering(class_name=self.__class_name, method_name=_method_name)
         model_deployer.deploy_model_after_update(self.model, self.model_context, self.aliases)
+        self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
 
     def __deploy_resources_and_apps(self):
         """
@@ -468,16 +449,6 @@ class DomainCreator(Creator):
 
         self.__configure_fmw_infra_database()
 
-        if self.wls_helper.is_set_server_groups_supported():
-            # 12c versions set server groups directly
-            server_groups_to_target = self._domain_typedef.get_server_groups_to_target()
-            self.target_helper.target_server_groups_to_servers(server_groups_to_target)
-
-        elif self._domain_typedef.is_jrf_domain_type() or \
-                (self._domain_typedef.get_targeting() == TargetingType.APPLY_JRF):
-            # for 11g, if template list includes JRF, or if specified in domain typedef, use applyJRF
-            self.target_helper.target_jrf_groups_to_clusters_servers()
-
         self.logger.info('WLSDPLY-12209', self._domain_name,
                          class_name=self.__class_name, method_name=_method_name)
 
@@ -545,21 +516,29 @@ class DomainCreator(Creator):
             self.__set_app_dir()
             self.__configure_fmw_infra_database()
 
-        self.logger.info('WLSDPLY-12206', self._domain_name, domain_home,
-                         class_name=self.__class_name, method_name=_method_name)
+        self.__configure_opss_secrets()
+        self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
+        return
 
+    def __set_server_groups(self):
+        _method_name = '__set_server_groups'
+        self.logger.entering(class_name=self.__class_name, method_name=_method_name)
         server_groups_to_target = self._domain_typedef.get_server_groups_to_target()
         server_assigns, dynamic_assigns = self.target_helper.target_server_groups_to_servers(server_groups_to_target)
         if len(server_assigns) > 0:
             self.target_helper.target_server_groups(server_assigns)
 
-        self.__configure_opss_secrets()
-
         if len(dynamic_assigns) > 0:
             self.target_helper.target_server_groups_to_dynamic_clusters(dynamic_assigns)
 
         self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
-        return
+
+    def __update_domain(self):
+        _method_name = '__update_domain'
+        self.logger.entering(class_name=self.__class_name, method_name=_method_name)
+        self.wlst_helper.update_domain()
+        self.wlst_helper.close_domain()
+        self.logger.exiting(class_name=self.__class_name, method_name=_method_name)
 
     def __apply_base_domain_config(self, topology_folder_list):
         """
