c3cd9d21e2c66c720946422c224d4fef124f4beb
Richard Killen <richard.killen@oracle.com>
2020-04-03 16:00:09 -0500

    Parsers check for lexical errors as well as parser errors

--- core/src/test/java/oracle/weblogic/deploy/yaml/YamlTranslatorTest.java
+++ core/src/test/java/oracle/weblogic/deploy/yaml/YamlTranslatorTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2020, Oracle Corporation and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
-package oracle.weblogic.deploy.json;
+package oracle.weblogic.deploy.yaml;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-public class JsonTranslatorTest {
+public class YamlTranslatorTest {
 
     /**
      * Verify that a lexical error will throw an Exception
@@ -22,19 +22,21 @@ public class JsonTranslatorTest {
     @Test
     public void testLexicalError() {
         // Temporarily disable logging for this test. Intended lexical failure will log a stack trace.
-        Logger logger = Logger.getLogger("wlsdeploy.json");
+        Logger logger = Logger.getLogger("wlsdeploy.yaml");
         Level originalLevel  = logger.getLevel();
         logger.setLevel(Level.OFF);
 
         try {
-            // JSON { "abc": "xyz"\ } causes lexical error
-            String text = "{ \"abc\": \"xyz\"/ }";
+            // YAML causes lexical error:
+            // abc:
+            //   xyz: aa-bb
+            String text = "abc:\n  xyz: aa-bb\n";
             InputStream stream = new ByteArrayInputStream(text.getBytes(UTF_8));
-            JsonStreamTranslator translator = new JsonStreamTranslator("String", stream);
+            YamlStreamTranslator translator = new YamlStreamTranslator("String", stream);
             translator.parse();
-            Assert.fail("Test must raise JsonException when model has a lexical error");
+            Assert.fail("Test must raise YamlException when model has a lexical error");
 
-        } catch(JsonException e) {
+        } catch(YamlException e) {
             // expected result
 
         } finally {
