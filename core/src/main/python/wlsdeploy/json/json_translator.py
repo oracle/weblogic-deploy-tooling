a2fef6ac102a567f6ceca72aee1a3ed4beff16fa
Richard Killen <richard.killen@oracle.com>
2020-10-12 14:51:51 -0500

    JIRA WDT-486 - Make separate case for boolean text, no longer auto-converted with newer Jython

--- core/src/main/python/wlsdeploy/json/json_translator.py
+++ core/src/main/python/wlsdeploy/json/json_translator.py
@@ -233,8 +233,10 @@ def _format_json_value(value):
     """
     import java.lang.StringBuilder as StringBuilder
     builder = StringBuilder()
-    if type(value) == bool or (isinstance(value, types.StringTypes) and (value == 'true' or value == 'false')):
+    if type(value) == bool:
         builder.append(JBoolean.toString(value))
+    elif isinstance(value, types.StringTypes) and (value == 'true' or value == 'false'):
+        builder.append(value)
     elif isinstance(value, types.StringTypes):
         builder.append('"').append(_escape_text(value.strip())).append('"')
     else:
