ee61ca717f870cde2f92759e429df24cc2f39e98
Richard Killen <richard.killen@oracle.com>
2020-10-27 18:25:51 -0500

    JIRA WDT-488 - Verify types and structure of WKO domain resource schema; build a complete model

--- core/src/main/python/wlsdeploy/tool/extract/wko_schema_helper.py
+++ core/src/main/python/wlsdeploy/tool/extract/wko_schema_helper.py
@@ -2,17 +2,32 @@
 Copyright (c) 2020, Oracle Corporation and/or its affiliates.
 Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 """
-
 from oracle.weblogic.deploy.util import FileUtils
 
 from wlsdeploy.exception import exception_helper
 from wlsdeploy.exception.expection_types import ExceptionType
 from wlsdeploy.json.json_translator import JsonStreamToPython
 from wlsdeploy.logging import platform_logger
+from wlsdeploy.util import dictionary_utils
 
 DOMAIN_RESOURCE_SCHEMA_ROOT = "openAPIV3Schema"
 DOMAIN_RESOURCE_SCHEMA_FILE = 'domain-crd-schemav7-260.json'
 DOMAIN_RESOURCE_SCHEMA_PATH = 'oracle/weblogic/deploy/wko/' + DOMAIN_RESOURCE_SCHEMA_FILE
+
+SIMPLE_TYPES = [
+    'number',
+    'string',
+    'boolean'
+]
+
+MULTI_KEYS = {
+    'spec/adminServer/adminService/channels': 'channelName',
+    'spec/clusters': 'clusterName'
+}
+
+UNSUPPORTED_FOLDERS = [
+    'status',
+]
 
 _logger = platform_logger.PlatformLogger('wlsdeploy.deploy')
 _class_name = 'wko_schema_helper'
@@ -42,3 +57,18 @@ def get_domain_resource_schema(exception_type=ExceptionType.DEPLOY):
             template_stream.close()
 
     return schema
+
+
+def get_mapped_key(path):
+    """
+    Because the WDT model does not support hyphenated lists, the name of each item in an object array must
+    sometimes corresponds to one of its attributes, usually "name".
+    If a different attribute name is used for the path, return that name.
+    If the default 'name' is returned, caller should verify that it is an available attribute.
+    :param path: the slash-delimited path of the elements
+    :return: the attribute key to be used
+    """
+    mapped_key = dictionary_utils.get_element(MULTI_KEYS, path)
+    if mapped_key is not None:
+        return mapped_key
+    return 'name'
