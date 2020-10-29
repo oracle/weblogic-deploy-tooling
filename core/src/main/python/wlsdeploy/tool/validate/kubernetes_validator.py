e84a30cbc883319ee4ade17fda52abfa47ed0a37
Richard Killen <richard.killen@oracle.com>
2020-10-29 10:04:23 -0500

    JIRA WDT-488 - Updated kubernetes validator to log more issues

--- core/src/main/python/wlsdeploy/tool/validate/kubernetes_validator.py
+++ core/src/main/python/wlsdeploy/tool/validate/kubernetes_validator.py
@@ -1,27 +1,13 @@
 """
-Copyright (c) 2017, 2020, Oracle Corporation and/or its affiliates.
+Copyright (c) 2020, Oracle Corporation and/or its affiliates.
 Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 """
 
-from wlsdeploy.aliases import model_constants
 from wlsdeploy.aliases.model_constants import KUBERNETES
 from wlsdeploy.exception.expection_types import ExceptionType
 from wlsdeploy.tool.extract import wko_schema_helper
-from wlsdeploy.tool.validate import validation_utils
 from wlsdeploy.tool.validate.validator_logger import ValidatorLogger
 from wlsdeploy.util import dictionary_utils
-from wlsdeploy.util.enum import Enum
-
-
-_ModelNodeTypes = Enum(['FOLDER_TYPE', 'NAME_TYPE', 'ATTRIBUTE', 'ARTIFICIAL_TYPE'])
-_ValidationModes = Enum(['STANDALONE', 'TOOL'])
-_ROOT_LEVEL_VALIDATION_AREA = validation_utils.format_message('WLSDPLY-05000')
-_DOMAIN_INFO_VALIDATION_AREA = validation_utils.format_message('WLSDPLY-05001', model_constants.DOMAIN_INFO)
-_TOPOLOGY_VALIDATION_AREA = validation_utils.format_message('WLSDPLY-05001', model_constants.TOPOLOGY)
-_RESOURCES_VALIDATION_AREA = validation_utils.format_message('WLSDPLY-05001', model_constants.RESOURCES)
-_APP_DEPLOYMENTS_VALIDATION_AREA = validation_utils.format_message('WLSDPLY-05001', model_constants.APP_DEPLOYMENTS)
-_GLOBAL_LEVEL_VARAIBLE_SUBSTITUTE = validation_utils.format_message('WLSDPLY-05001',
-                                                                    model_constants.GLOBAL_VARIABLE_SUBSTITUTION)
 
 
 class KubernetesValidator(object):
@@ -53,14 +39,21 @@ class KubernetesValidator(object):
 
         self.validate_folder(kubernetes_section, schema, None)
 
-    def validate_folder(self, model_folder, schema_folder, path, extra_attributes=None):
+    def validate_folder(self, model_folder, schema_folder, path):
         """
         Validate the specified model folder against the specified schema folder
+        :param model_folder: the model folder to validate
+        :param schema_folder: the schema folder to validate against
+        :param path: the path of model elements, used for logging
         """
-        # if extra_attributes is None:
-        #     extra_attributes = {}
-
+        _method_name = 'validate_folder'
         self._log_debug(str(path))
+
+        log_path = _get_log_path(path)
+
+        if not isinstance(model_folder, dict):
+            self._logger.severe("WLSDPLY-05038", log_path, class_name=self._class_name, method_name=_method_name)
+            return
 
         properties = dictionary_utils.get_dictionary_element(schema_folder, "properties")
 
@@ -77,7 +70,7 @@ class KubernetesValidator(object):
                     if additional_type:
                         # map of key / value pairs
                         self._log_debug('  ' + key + ': map of ' + additional_type)
-                        self._validate_simple_map(model_value, property_map)
+                        self._validate_simple_map(model_value, key, log_path)
                     else:
                         # single object instance
                         self._log_debug('  ' + key + ': folder')
@@ -95,32 +88,45 @@ class KubernetesValidator(object):
                     else:
                         # array of simple type
                         self._log_debug('  ' + key + ': array of ' + array_type)
-                        self._validate_simple_array(model_value, property_map)
+                        self._validate_simple_array(model_value, key, log_path)
 
                 else:
                     self._log_debug('  ' + key + ': ' + property_type)
-                    self._validate_simple_type(model_value, property_type)
+                    self._validate_simple_type(model_value, property_type, key, log_path)
 
             else:
-                self._log_debug('  *** ' + key + " not in " + str(properties))
+                self._logger.severe("WLSDPLY-05026", key, len(properties), log_path, class_name=self._class_name,
+                                    method_name=_method_name)
 
-    def _validate_simple_map(self, model_value, property_map):
-        pass
+    def _validate_simple_map(self, model_value, property_name, log_path):
+        _method_name = '_validate_simple_map'
+        if not isinstance(model_value, dict):
+            self._logger.severe("WLSDPLY-05032", property_name, log_path, str(type(model_value)),
+                                class_name=self._class_name, method_name=_method_name)
 
     def _validate_multiple_folder(self, model_value, property_map, path):
+        _method_name = '_validate_multiple_folder'
         if not isinstance(model_value, dict):
-            self._logger.severe("expecting dict at " + path)
+            self._logger.severe("WLSDPLY-05039", _get_log_path(path), class_name=self._class_name,
+                                method_name=_method_name)
+            return
 
         for name in model_value:
             name_map = model_value[name]
             next_path = _get_next_path(path, name)
             self.validate_folder(name_map, property_map, next_path)
 
-    def _validate_simple_array(self, model_value, property_map):
-        pass
+    def _validate_simple_array(self, model_value, property_name, log_path):
+        _method_name = '_validate_simple_array'
+        if isinstance(model_value, dict):
+            self._logger.severe("WLSDPLY-05017", property_name, log_path, "list", str(type(model_value)),
+                                class_name=self._class_name, method_name=_method_name)
 
-    def _validate_simple_type(self, model_value, property_type):
-        pass
+    def _validate_simple_type(self, model_value, property_type, property_name, log_path):
+        _method_name = '_validate_simple_type'
+        if isinstance(model_value, list) or isinstance(model_value, dict):
+            self._logger.severe("WLSDPLY-05017", property_name, log_path, property_type, str(type(model_value)),
+                                class_name=self._class_name, method_name=_method_name)
 
     def _log_debug(self, message):
         self._logger.finest(message)
@@ -130,3 +136,10 @@ def _get_next_path(path, key):
     if path is None:
         return key
     return path + '/' + key
+
+
+def _get_log_path(path):
+    log_path = KUBERNETES + ':/'
+    if path:
+        log_path = log_path + path
+    return log_path
