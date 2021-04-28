0c8a184091a490066d7870341b17d344573e7fec
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-04-28 17:03:57 -0400

    Correct configuration

--- documentation/1.9/runlocal.sh
+++ documentation/1.9/runlocal.sh
@@ -6,4 +6,4 @@
 # 1313 is the hugo default port
 port=${1:-1313}
 
-hugo server -b http://localhost:$port/weblogic-kubernetes-operator -D -p $port
+hugo server -b http://localhost:$port/weblogic-deploy-tooling -D -p $port
--- documentation/1.9/runpublic.sh
+++ documentation/1.9/runpublic.sh
@@ -6,4 +6,4 @@
 # 1313 is the hugo default port
 port=${1:-1313}
 
-hugo server -b http://$(hostname).$(dnsdomainname):$port/weblogic-kubernetes-operator -D --bind=$(hostname).$(dnsdomainname) -p $port
+hugo server -b http://$(hostname).$(dnsdomainname):$port/weblogic-deploy-tooling -D --bind=$(hostname).$(dnsdomainname) -p $port
