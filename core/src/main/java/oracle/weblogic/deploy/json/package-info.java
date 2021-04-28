1bd9c21b5dbba2047938ecedad66d3a0365aa2b2
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-04-28 16:58:07 -0400

    Initial documentation for gh-pages

--- core/src/main/java/oracle/weblogic/deploy/json/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2017, 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
- * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
- */
-/**
- * JSON Parser-related classes.
- */
-package oracle.weblogic.deploy.json;
