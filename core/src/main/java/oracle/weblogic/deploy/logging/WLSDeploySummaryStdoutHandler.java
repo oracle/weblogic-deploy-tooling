b2a617e13a34898709b5ee92e0d24cc6af0e9692
Carolyn Rountree <carolyn.rountree@oracle.com>
2022-02-02 15:51:50 -0600

    copyright

--- core/src/main/java/oracle/weblogic/deploy/logging/WLSDeploySummaryStdoutHandler.java
+++ core/src/main/java/oracle/weblogic/deploy/logging/WLSDeploySummaryStdoutHandler.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022, Oracle Corporation and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
 package oracle.weblogic.deploy.logging;
 
 import java.util.logging.LogRecord;
