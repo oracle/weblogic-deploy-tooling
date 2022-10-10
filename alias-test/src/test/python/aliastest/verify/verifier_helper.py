86fd25a75b1717fa2de1f53035814b7986715404
Carolyn Rountree <carolyn.rountree@oracle.com>
2022-10-10 14:35:06 -0500

    push out of wrong branch

--- alias-test/src/test/python/aliastest/verify/verifier_helper.py
+++ alias-test/src/test/python/aliastest/verify/verifier_helper.py
@@ -326,11 +326,6 @@ class VerifierHelper:
         :param location: current location in aliases
         """
         _method_name = 'verify_attributes'
-        flattened_info = self._helper.aliases().get_wlst_flattened_folder_info(location)
-        if flattened_info is not None:
-            path_token = flattened_info.get_path_token()
-            mbean_name = flattened_info.get_mbean_name()
-            location.add_name_token(path_token, mbean_name)
         model_name_map = self._model_attribute_map(location)
         _logger.fine('WLSDPLYST-01211', location.get_folder_path(), model_name_map,
                      class_name=CLASS_NAME, method_name=_method_name)
@@ -978,9 +973,6 @@ class VerifierHelper:
         if flattened_info is not None:
             _logger.finer('WLSDPLYST-01203', location.get_folder_path(), class_name=CLASS_NAME,
                           method_name=_method_name)
-            path_token = flattened_info.get_path_token()
-            mbean_name = flattened_info.get_mbean_name()
-            location.add_name_token(path_token, mbean_name)
         else:
             keys = folder_map.keys()
             lower_case_map = all_utils.get_lower_case_dict(dictionary.keys())
