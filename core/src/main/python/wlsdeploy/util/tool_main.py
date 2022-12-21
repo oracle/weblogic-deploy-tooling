a036f45056e4deff9125996955846e4eb02de0af
Robert Patrick <robert.patrick@oracle.com>
2022-12-21 12:56:06 -0600

    changing tool exit codes to reflect summary handler results

--- core/src/main/python/wlsdeploy/util/tool_main.py
+++ core/src/main/python/wlsdeploy/util/tool_main.py
@@ -7,8 +7,10 @@ import sys
 import traceback
 
 from java.lang import Exception as JException
+from java.util.logging import Level as JLevel
 
 from oracle.weblogic.deploy.logging import WLSDeployLoggingConfig
+from oracle.weblogic.deploy.logging import WLSDeployLogEndHandler
 from oracle.weblogic.deploy.util import CLAException
 from oracle.weblogic.deploy.util import WebLogicDeployToolingVersion
 from oracle.weblogic.deploy.util import WLSDeployExit
@@ -20,6 +22,7 @@ from wlsdeploy.tool.util import model_context_helper
 from wlsdeploy.util import cla_helper
 import wlsdeploy.util.unicode_helper as str_helper
 from wlsdeploy.util.exit_code import ExitCode
+
 
 def run_tool(main, process_args, args, program_name, class_name, logger):
     """
@@ -78,6 +81,9 @@ def __exit_tool(model_context, exit_code):
         version = model_context.get_target_wls_version()
         if model_context.get_target_wlst_mode() == WlstModes.ONLINE:
             tool_mode = JWLSTMode.ONLINE
+
+    exit_code = __get_summary_handler_exit_code(exit_code)
+
     WLSDeployExit.exit(WLSDeployContext(program, version, tool_mode), exit_code)
 
 
@@ -107,3 +113,25 @@ def __handle_unexpected_exception(ex, model_context, class_name, method_name, lo
         # and of course only while in the except block handling code
         logger.fine('WLSDPLY-20036', program_name, traceback.format_exception(type(ex), ex, sys.exc_info()[2]),
                     class_name=class_name, method_name=method_name, error=ex)
+
+
+def __get_summary_handler_exit_code(program_exit_code):
+    """
+    Private method for use only within this module.
+
+    Helper method to get the proper tool exit code based on the exit code from the tool and the number
+    of errors and warnings from the summary log handler.
+    :param program_exit_code: the exit code from the tool
+    :return: the exit code to use
+    """
+    if program_exit_code != ExitCode.OK:
+        return program_exit_code
+
+    exit_code = ExitCode.OK
+    summary_handler = WLSDeployLogEndHandler.getSummaryHandler()
+    if summary_handler is not None:
+        if summary_handler.getMessageCount(JLevel.SEVERE) > 0:
+            exit_code = ExitCode.ERROR
+        elif summary_handler.getMessageCount(JLevel.WARNING) > 0:
+            exit_code = ExitCode.WARNING
+    return exit_code
