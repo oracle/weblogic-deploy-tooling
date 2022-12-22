4c79d9162c895d260944239d48d92c37e51ff9a0
Robert Patrick <robert.patrick@oracle.com>
2022-12-21 21:00:12 -0600

    removing unused imports

--- core/src/main/java/oracle/weblogic/deploy/util/XPathUtil.java
+++ core/src/main/java/oracle/weblogic/deploy/util/XPathUtil.java
@@ -12,9 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
