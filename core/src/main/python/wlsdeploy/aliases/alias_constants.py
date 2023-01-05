959a0ba38aed02e2f04144aca977e4cdccd332c9
Richard Killen <richard.killen@oracle.com>
2023-01-05 17:08:58 -0600

    Updated copyrights

--- core/src/main/python/wlsdeploy/aliases/alias_constants.py
+++ core/src/main/python/wlsdeploy/aliases/alias_constants.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2017, 2022, Oracle Corporation and/or its affiliates.
+Copyright (c) 2017, 2023, Oracle Corporation and/or its affiliates.
 Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 These constants are internal to the aliases module and should not be used, as they are not part of the public API.
